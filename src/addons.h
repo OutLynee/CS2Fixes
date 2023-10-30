629fe3b1bd0d4b172e5b90c34794ae8218bb1282
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2023-10-30 19:56:34 +0200

    Update addons.h

--- src/addons.h
+++ src/addons.h
@@ -135,7 +135,7 @@ CON_COMMAND_CHAT(medic, "medic")
 	pZEPlayer->SetUsedMedkit(true);
 
 	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"Medkit used! Your health is now %d", health);
-	g_pEngineServer2->ClientCommand(player->GetPlayerSlot(), "play ui/cs_info");
+	g_pEngineServer2->ClientCommand(player->GetPlayerSlot(), "play items/healthshot_success_01");
 }
 CON_COMMAND_CHAT(stats, "get your stats")
 {
